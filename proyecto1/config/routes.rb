f462243712c60f1290fa011c6da139d12fcc5ce8
ledferoz <ledferoz@gmail.com>
2018-08-23 21:15:58 -0500

    aaa

--- proyecto1/config/routes.rb
+++ proyecto1/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  resources : posts
-  resources : images
-  root 'welcome/index'
+  resources :posts
+  resources :images
+  root 'welcome#index'
 end
 
 
--- proyecto1/db/migrate/20180822011524_create_images.rb
+++ proyecto1/db/migrate/20180822011524_create_images.rb
@@ -3,7 +3,6 @@ class CreateImages < ActiveRecord::Migration[5.0]
     create_table :images do |t|
       t.string :description
       t.integer :likes_counter
-
       t.timestamps
     end
   end
